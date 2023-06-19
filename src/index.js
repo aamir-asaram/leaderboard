6caaa062dd33361df4a0f5822adb70d6a62e1511
Aamir <aamirasaram2107@gmail.com>
2023-06-19 10:57:38 +0200

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const postScore = async (user, score) => {
   try {
     await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
       method: 'POST',
-      body: JSON.stringify({user, score}),
+      body: JSON.stringify({ user, score }),
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
